a05540ddbeb98c1decdb55dfe01acaa178400b7c
Rommy Elyan <relyan@email.arizona.edu>
2018-04-09 21:50:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,26 +5,56 @@ function getCart() {
 }
 
 function setCart(c) {
-  cart = c;
+  cart=c;
   return cart;
 }
 
 function addToCart(item) {
- // write your code here
+  var itemName = {};
+  itemName.item=item;
+  itemName.price=Math.floor(Math.random()*101);
+  cart.push(itemName);
+  return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  var newCart=[];
+  if (cart.length===0){
+    return 'Your shopping cart is empty.'
+  }else if (cart.length===1) 
+  {for (var i = 0;i<cart.length;i++){
+  newCart.push(`${cart[i].item} at $${cart[i].price}`)
+  }return `In your cart, you have ${newCart}.`
+}else if (cart.length===2){
+  for (let i=0;i<cart.length;i++){
+  newCart.push(`${cart[i].item} at $${cart[i].price}`)  
+  }return `In your cart, you have ${newCart.join(', and ')}.`
+}else if (cart.length>2){
+  for (let i = 0;i<cart.length;i++){
+  newCart.push(`${cart[i].item} at $${cart[i].price}`)  
+  }return 'In your cart, you have '+newCart.slice(0,-1).join(', ') +', and ' + newCart.slice(-1)+'.';
+  }
 }
 
 function total() {
-  // write your code here
+var current=[];
+for (let i=0;i<cart.length;i++){
+  current.push(cart[i].price)}
+  var totals = current.reduce(( x, y ) => x + y, 0);
+  return totals
 }
 
 function removeFromCart(item) {
-  // write your code here
+  
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber===undefined){
+    return "Sorry, we don't have a credit card on file for you."
+}else
+
+console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+cart.length=0;
+  return;
 }
+
