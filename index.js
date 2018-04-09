ebcb3fa820f555dbc52c5d6c698294a01e22ee40
Rommy Elyan <relyan@email.arizona.edu>
2018-04-09 22:24:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ for (let i=0;i<cart.length;i++){
 function removeFromCart(item) {
 for (let i=cart.length-1;i>=0; --i) {
     if (cart[i].itemName===item) {
-        
-    return cart.splice(i,1);
+        cart.splice(i,1);
+    return cart;
 }else if (item===undefined)
 return 'That item is not in your cart.'
   }
