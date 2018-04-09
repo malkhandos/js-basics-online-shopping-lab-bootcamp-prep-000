8dc4f3185983d2371fef0c9dce075b3ac6bb22ef
Rommy Elyan <relyan@email.arizona.edu>
2018-04-09 22:11:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,12 +55,10 @@ return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber===undefined){
-    return "Sorry, we don't have a credit card on file for you."
+  if (cardNumber!==undefined){
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }else
 
-console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-cart.length=0;
-  return;
+return 
 }
 
